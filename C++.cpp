a20a3848228c28ae2530054047ee4e9f3a8a4253
Karnav5622 <karnav5622@gmail.com>
2026-01-04 16:45:06 +0530

    Inital commit

---
+++ C++.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+// int main(){
+//     std::cout<<"hello world";
+//     return 0;
+// }
+int main(){
+    int a=12,b=3;
+    int sum=a+b;
+    std::cout<<sum;
+    
+    return 0;
+}
---
+++ tut1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter number 1\n";
+    cin>>a;
+    cout<<"Enter number 2\n";
+    cin>>b;
+    int multi;
+    multi=a*b;
+    cout<<"Multiplication of two number will be\n"<<multi;
+    
+    return 0;
+}
