5b500b6dc06ff24cfa1101640779eb085fce9a65
Karnav5622 <karnav5622@gmail.com>
2026-01-05 15:48:07 +0530

    Updated Readme.md simpecode.cpp

--- Basics/Readme.md
+++ Basics/Readme.md
@@ -1,1 +1,12 @@
-Contains basics codes/topic of what I have learnt<br>
+## Basics of C++
+
+Contains basic codes and topics that I have learned.
+
+### What I have done till now:
+
+1. C++ introduction / origin  
+2. Standardization and basic terminology  
+3. Different versions of C++  
+4. Comments in C++  
+5. Variables and constants in C++
+
--- Basics/simplecode.cpp
+++ Basics/simplecode.cpp
@@ -1,21 +1,21 @@
 //Accessing and updating a variable in c
 #include<iostream>
 using namespace std;
-// int main(){
-//     int num,x;
-//     cout<<"Enter a number\n";
-//     cin>>num;
-//     cout<<"Enter a number to update\n";
-//     cin>>x;
-//     num=x;
-//     cout<<"Updated number is\n"<<num;
-//     return 0;
-// }
-//Concept of Constant variable
 int main(){
-    const int a=100;
-    cout<<"Enter a number";
-    a=200;//Generate error due to a is constant
-    cout<<a;
+    int num,x;
+    cout<<"Enter a number\n";
+    cin>>num;
+    cout<<"Enter a number to update\n";
+    cin>>x;
+    num=x;
+    cout<<"Updated number is\n"<<num;
     return 0;
 }
+//Concept of Constant variable
+// int main(){
+//     const int a=100;
+//     cout<<"Enter a number";
+//     a=200;//Generate error due to a is constant
+//     cout<<a;
+//     return 0;
+// }
